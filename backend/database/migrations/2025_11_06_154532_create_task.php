77c66ce01698a2fff4ae04df636d0466f8765684
RianHidayatSaputra <jendraltengue123@gmail.com>
2025-11-06 22:54:26 +0700

    add migration task

---
+++ backend/database/migrations/2025_11_06_154532_create_task.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id('task_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['todo', 'in_progress', 'done']);
+            $table->date('deadline');
+            $table->string('created_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task');
+    }
+};
