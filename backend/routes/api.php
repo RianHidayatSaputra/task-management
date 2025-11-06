d5ab6dcf4fe6df628c4bf800df1108f17adf9620
RianHidayatSaputra <jendraltengue123@gmail.com>
2025-11-06 22:43:36 +0700

    change route structure

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,15 +1,22 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function () {
-
+Route::prefix('auth')->middleware('api')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+});
+
+Route::middleware(['api', 'auth:api'])->group(function () {
+    Route::get('users', function () {
+        $users = User::all();
+        return response()->json([
+            'message' => 'success!',
+            'data' => $users
+        ]);
+    });
 });
